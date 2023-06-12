5b3c600b95ebca3304cc152c8cb087eca69d9de8
marcofjaja <135852448+marcofjaja@users.noreply.github.com>
2023-06-12 09:57:26 -0700

    Create master.py

---
+++ master.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+from ventana import *
+
+def main():
+    root=Tk()
+    root.wm_title("Crud tkinter y Mysql")
+    app=Ventana(root)
+    app.mainloop()
+    
+if __name__=="__main__":
+    main()
